78133752a2cd5a3f276d402af96c62f2570f8ae1
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-11-04 00:57:11 -0700

    Update momoball.js

--- commands/momoball.js
+++ commands/momoball.js
@@ -2,7 +2,17 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("Momo-chan doesn't understand what you just said! (∩´ ∀`)∩☆彡");
-   let replies = ["Of course! (。≧ω≦)ノ", "That’s right~! ｡\*(\*´∀`\*)\*｡゜゜", "Ah! That's not right! (\*´艸`*)☆彡", " Don’t say that… oyoyo… ;つд`)", "Hmm... Probably not.", "You're kidding, right?", "Nn... I guess you're right ;つд`)", "No way! Momo-chan won't allow it! <:myubutangery:454109952441712640>", "It's fine. You worry too much."];
+   let replies = [
+     "Of course! (。≧ω≦)ノ",
+     "That’s right~! ｡\*(\*´∀`\*)\*｡゜゜",
+     "Ah! That's not right! (\*´艸`*)☆彡",
+     "Don’t say that… oyoyo… ;つд`)",
+     "Hmm... Probably not.",
+     "You're kidding, right?",
+     "Nn... I guess you're right ;つд`)",
+     "No way! Momo-chan won't allow it! <:myubutangery:454109952441712640>",
+     "It's fine. You worry too much."
+ ];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
@@ -14,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
 
    let botschannel = message.guild.channels.find(`name`, "bot-channel");
    if(!botschannel) return;
-   botschannel.send(ballembed);
+   return botschannel.send(ballembed);
 }
 
 module.exports.help = {
