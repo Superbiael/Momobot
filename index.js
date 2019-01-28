588e3a675a3ffb17ef89af89d4be461ef816b8f5
Superbiael <39025166+Superbiael@users.noreply.github.com>
2019-01-27 21:15:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ bot.on('message', message => {
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args);
 
-       let botschannel = message.guild.channels.find(`name`, "bot-channel");
-       if(!botschannel) return;
+//   Redirect to a specific channel
+   let botschannel = message.guild.channels.find(channel => channel.name === 'bot-channel');
+   if(!botschannel) return;
 
      msg = message.content.toLowerCase();
      mention = message.mentions.users.first();
